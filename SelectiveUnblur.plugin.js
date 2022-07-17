b372e5e11ca38f6a3649f5541874321b155cf467
Kyosukyuu <82899136+Kyosukyuu@users.noreply.github.com>
2022-07-16 20:28:01 -0400

    updated config object

--- SelectiveUnblur.plugin.js
+++ SelectiveUnblur.plugin.js
@@ -19,7 +19,7 @@ module.exports = (() => {
           name: "Kyoskyuu",
         },
       ],
-      version: "0.0.1",
+      version: "0.0.2",
       description: "Unblur certain channels of your choosing",
     },
     changelog: [],
